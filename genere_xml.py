400bd89d591463ed58577bcbb1b8b0155adaaf4b
gnoiret <gregoire.alizadehnoiret@gmail.com>
2021-11-14 23:55:22 +0100

    Correction dico

--- genere_xml.py
+++ genere_xml.py
@@ -36,27 +36,27 @@ import base64
 
 def dna_to_prot(dna_seq:str):
     matches = {
-        'AAA':'K', 'AAC':'N', 'AAG':'K', 'AAT':'N'
-        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T'
-        'AGA':'R', 'AGC':'S', 'AGG':'R', 'AGT':'S'
-        'ATA':'I', 'ATC':'I', 'ATG':'M', 'ATT':'I'
+        'AAA':'K', 'AAC':'N', 'AAG':'K', 'AAT':'N',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AGA':'R', 'AGC':'S', 'AGG':'R', 'AGT':'S',
+        'ATA':'I', 'ATC':'I', 'ATG':'M', 'ATT':'I',
 
-        'CAA':'Q', 'CAC':'H', 'CAG':'Q', 'CAT':'H'
-        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P'
-        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R'
-        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L'
+        'CAA':'Q', 'CAC':'H', 'CAG':'Q', 'CAT':'H',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
 
-        'GAA':'E', 'GAC':'D', 'GAG':'E', 'GAT':'D'
-        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A'
-        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G'
-        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V'
+        'GAA':'E', 'GAC':'D', 'GAG':'E', 'GAT':'D',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
 
-        'TAA':'*', 'TAC':'Y', 'TAG':'*', 'TAT':'Y'
-        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S'
-        'TGA':'*', 'TGC':'C', 'TGG':'W', 'TGT':'C'
-        'TTA':'L', 'TTC':'F', 'TTG':'L', 'TTT':'F'
+        'TAA':'*', 'TAC':'Y', 'TAG':'*', 'TAT':'Y',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TGA':'*', 'TGC':'C', 'TGG':'W', 'TGT':'C',
+        'TTA':'L', 'TTC':'F', 'TTG':'L', 'TTT':'F',
 
-        '---':'-',
+        '---':'-'
     }
     aa = ''
     if len(dna_seq)%3 == 0:
